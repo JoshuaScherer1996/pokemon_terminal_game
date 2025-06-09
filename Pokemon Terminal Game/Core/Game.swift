55483d16ad685b5a13729d64a1d9ada9e3009f4c
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-06-09 19:50:10 +0200

    added the basic folder structure

---
+++ Pokemon Terminal Game/Core/Game.swift
@@ -0,0 +1,5 @@
+// File for main game Logic
+
+func startGame() {
+    print("Getting back into things!")
+}
--- Pokemon Terminal Game/main.swift
+++ Pokemon Terminal Game/main.swift
@@ -1,3 +1,6 @@
 import Foundation
 
 
+
+// Starting point of the Application. Calls the startGame function where everything begins.
+startGame()
