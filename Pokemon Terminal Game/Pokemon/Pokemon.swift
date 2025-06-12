5811718f62c654d2661d3378648a049512e2e9f0
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-06-12 17:08:34 +0200

    Finished the data modelling for the Pokemons inside the Pokemon.swift file

--- Pokemon Terminal Game/Pokemon/Pokemon.swift
+++ Pokemon Terminal Game/Pokemon/Pokemon.swift
@@ -1,5 +1,14 @@
 // MARK: Pokemon data model
 
 struct Pokemon {
+    let id: Int
+    let name: String
+    let type: [PokeType]
     
+    let maxHP: Int
+    var currentHp: Int
+    
+    let catchDifficulty: Int
+    
+    // Later on: Implementation of functions through protocols
 }
