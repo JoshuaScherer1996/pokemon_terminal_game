8a8312695d6f6cc76a0404b63f4950e4d1392d4b
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-06-12 16:57:41 +0200

    finished moddeling all the different types for the Kanto region pokemon inside the PokeType.swift file and added the functionality for raw values of type String

--- Pokemon Terminal Game/Pokemon/PokeType.swift
+++ Pokemon Terminal Game/Pokemon/PokeType.swift
@@ -1,5 +1,20 @@
 // MARK: Different Pokemon Types
 
-enum PokeType {
-    
+// Enum lists all the different possible types with a raw value of type string
+enum PokeType: String {
+    case normal
+    case fire
+    case water
+    case grass
+    case electric
+    case ice
+    case fighting
+    case poison
+    case ground
+    case flying
+    case psychic
+    case bug
+    case rock
+    case ghost
+    case dragon
 }
