e1784112bb29b6b0bf7de9063ebc74a8a49ac241
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-06-14 16:40:02 +0200

    switched from the apple swift testing framework to XCTest so that the code is colser to the industry standard. Also implemented the skeletons functions for my test cases inside the PokemonTerminalGameTests.swift file alongside with my todos for the test cases

--- PokemonTerminalGameTests/PokemonTerminalGameTests.swift
+++ PokemonTerminalGameTests/PokemonTerminalGameTests.swift
@@ -1,16 +1,105 @@
-//
-//  PokemonTerminalGameTests.swift
-//  PokemonTerminalGameTests
-//
-//  Created by Joshua Scherer on 14.06.25.
-//
+import XCTest
+@testable import Pokemon_Terminal_Game
 
-import Testing
-
-struct PokemonTerminalGameTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+final class PokemonTerminalGameTests: XCTestCase {
+    
+    // MARK: - Capture Logic
+    
+    func testCatchChanceReturnsMinimumWhenDifficultyIsHigh() {
+        // TODO: Implement the test case
     }
 
+    func testCatchChanceReturnsMaximumWhenDifficultyIsLow() {
+        // TODO: Implement the test case
+    }
+
+    func testCatchChanceReturnsMidRangeCorrectly() {
+        // TODO: Implement the test case
+    }
+
+    func testCatchChanceNeverBelowMinimum() {
+        // TODO: Implement the test case
+    }
+
+    func testCatchChanceNeverAboveMaximum() {
+        // TODO: Implement the test case
+    }
+    
+    // MARK: - Battle Logic
+    
+    func testTakeDamageReducesCurrentHP() {
+        // TODO: Implement the test case
+    }
+
+    func testTakeDamageReducesHPToZero() {
+        // TODO: Implement the test case
+    }
+
+    func testTakeDamageCannotGoBelowZero() {
+        // TODO: Implement the test case
+    }
+
+    func testIsFaintedReturnsTrueWhenHPIsZero() {
+        // TODO: Implement the test case
+    }
+
+    func testIsFaintedReturnsFalseWhenHPAboveZero() {
+        // TODO: Implement the test case
+    }
+
+    
+    // MARK: - Factory Creation
+    
+    func testFactoryCreatesAllPokemon() {
+        // TODO: Implement the test case
+    }
+
+    func testFactorySkipsInvalidTypesGracefully() {
+        // TODO: Implement the test case
+    }
+
+    func testFirstPokemonHasCorrectValues() {
+        // TODO: Implement the test case
+    }
+
+    func testAllCreatedPokemonHaveValidIDs() {
+        // TODO: Implement the test case
+    }
+
+    func testAllCreatedPokemonHaveValidTypes() {
+        // TODO: Implement the test case
+    }
+    
+    //MARK: - Type Parsing
+    
+    func testSingleTypeIsParsedCorrectly() {
+        // TODO: Implement the test case
+    }
+
+    func testDualTypeIsParsedCorrectly() {
+        // TODO: Implement the test case
+    }
+
+    func testInvalidTypeReturnsNil() {
+        // TODO: Implement the test case
+    }
+
+    func testEmptyTypeReturnsNil() {
+        // TODO: Implement the test case
+    }
+    
+    //MARK: - Initialization
+    
+    func testCurrentHPIsInitializedAsMaxHP() {
+        // TODO: Implement the test case
+    }
+
+    func testPokemonInitializationWithCorrectValues() {
+        // TODO: Implement the test case
+    }
+
+    func testPokemonStructHoldsCorrectTypeData() {
+        // TODO: Implement the test case
+    }
+    
 }
