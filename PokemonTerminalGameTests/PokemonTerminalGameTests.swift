edf81c8d963c3d74f169803151198958f15b7b4d
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-06-14 15:48:35 +0200

    created the first testing bundle for the game

---
+++ PokemonTerminalGameTests/PokemonTerminalGameTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonTerminalGameTests.swift
+//  PokemonTerminalGameTests
+//
+//  Created by Joshua Scherer on 14.06.25.
+//
+
+import Testing
+
+struct PokemonTerminalGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
