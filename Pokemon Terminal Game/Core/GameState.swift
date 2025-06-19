7ba6a3f4f1aea3cf7f7eb56e4cbf8dedd65f01a5
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-06-19 12:46:24 +0200

    created both the GameState.swift file and the GameController.swift file. Also implemented the basic cases for the game states inside the GameState.swift file

---
+++ Pokemon Terminal Game/Core/GameState.swift
@@ -0,0 +1,15 @@
+// MARK: Enum representing the different game states
+
+/// Describes all major states the game can be in.
+enum GameState {
+    case welcome
+    case mainMenu
+    case playMenu
+    case pokedex
+    case encounter
+    case battle
+    case credits
+    case gameOver
+    case saveAndExit
+    case exit
+}
