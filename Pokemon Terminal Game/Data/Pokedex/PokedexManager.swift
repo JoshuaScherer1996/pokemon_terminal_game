35189ab645a28b3fd3004f54c0497aaec5767eb8
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-07-06 20:09:04 +0200

    started the migration to state pattern with creating a deprecated folder. This way I don't need to work with seperat branches that later need to be merged and the Program is still executeable during the migration process

--- Pokemon Terminal Game/Data/Pokedex/PokedexManager.swift
+++ Pokemon Terminal Game/Data/Pokedex/PokedexManager.swift
@@ -1,18 +1,34 @@
 import Foundation
 
+// MARK: Manager for tracking caught Pokémon in the Pokédex
+
+/// A manager that tracks all available Pokemon and handles visibility based on which have been caught.
+/// Used to build and display the Pokedex entries based on caught Pokemon IDs.
 final class PokedexManager {
+    /// A private list created with the factory containing all valid Pokemon from the first generation.
     private let allPokemon: [Pokemon] = PokeFactory.allPokemon()
     
+    /// A set of Pokemon IDs (as String) representing all Pokemon caught by the player.
     private(set) var caughtIDs: Set<String> = []
     
+    /// Initializes a new Pokedex manager instance with a starter Pokemon already marked as caught.
+    ///
+    /// - Parameter starter: The initially chosen starter Pokemon.
     init(starter: Pokemon) {
         caughtIDs.insert(starter.id)
     }
     
+    /// Marks a given Pokemon as caught and adds its ID to the set.
+    ///
+    /// - Parameter pokemon: The `Pokemon` instance that has just been caught.
     func gotCaught(_ pokemon: Pokemon) {
         caughtIDs.insert(pokemon.id)
     }
     
+    /// Returns all Pokedex entries, making only caught Pokemon visible.
+    /// Uncaught Pokemon are shown with question marks as placeholder values.
+    ///
+    /// - Returns: An array of `PokedexEntry` objects, with actual data for caught Pokemon and placeholders for the rest.
     func showEntries() -> [PokedexEntry] {
             return allPokemon.map { pokemon in
                 if caughtIDs.contains(pokemon.id) {
--- Pokemon Terminal Game/main.swift
+++ Pokemon Terminal Game/main.swift
@@ -1,14 +1,3 @@
 // MARK: Application Entry Point
 
-/// Entry point that starts the game logic.
-//startGame()
 
-
-//let controller = GameController()
-//controller.run()
-
-let pokedex = PokedexManager(starter: Pokemon(id: "001", name: "Bulbasaur", type: [.grass, .poison], maxHP: 45, attack: 49, catchDifficulty: 30))
-
-let entries = pokedex.showEntries()
-
-print(entries)
