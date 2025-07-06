2f9d02579a0eeef379eb2da192df42a62ab839b3
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-07-06 18:11:54 +0200

    implemented most of the logic for the PokedexManager including the allPokemon property which calls the allPokemon method of the Pokefactory thus saving all possible pokemon instances, created the caughtIds set which is meant to hold newly caught pokemon's id's, the initializer for the manager which immediatly adds the starter pokemon to the caughtIDs, the gotCaught method which ads the id of the caught pokemon to caughtIDs and the showEntries method which creates all needed Entries for the pokedex

--- Pokemon Terminal Game/Data/Pokedex/PokedexManager.swift
+++ Pokemon Terminal Game/Data/Pokedex/PokedexManager.swift
@@ -1,1 +1,36 @@
+import Foundation
 
+final class PokedexManager {
+    private let allPokemon: [Pokemon] = PokeFactory.allPokemon()
+    
+    private(set) var caughtIDs: Set<String> = []
+    
+    init(starter: Pokemon) {
+        caughtIDs.insert(starter.id)
+    }
+    
+    func gotCaught(_ pokemon: Pokemon) {
+        caughtIDs.insert(pokemon.id)
+    }
+    
+    func showEntries() -> [PokedexEntry] {
+            return allPokemon.map { pokemon in
+                if caughtIDs.contains(pokemon.id) {
+                    let typeString = pokemon.type
+                        .map { $0.rawValue.capitalized }
+                        .joined(separator: "/")
+                    return PokedexEntry(
+                        id: Int(pokemon.id) ?? 0,
+                        name: pokemon.name,
+                        type: typeString
+                    )
+                } else {
+                    return PokedexEntry(
+                        id: Int(pokemon.id) ?? 0,
+                        name: "???",
+                        type: "???"
+                    )
+                }
+            }
+        }
+}
--- Pokemon Terminal Game/main.swift
+++ Pokemon Terminal Game/main.swift
@@ -4,5 +4,11 @@
 //startGame()
 
 
-let controller = GameController()
-controller.run()
+//let controller = GameController()
+//controller.run()
+
+let pokedex = PokedexManager(starter: Pokemon(id: "001", name: "Bulbasaur", type: [.grass, .poison], maxHP: 45, attack: 49, catchDifficulty: 30))
+
+let entries = pokedex.showEntries()
+
+print(entries)
