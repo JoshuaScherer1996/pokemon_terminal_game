8c5debacb892a48b6d72bab971d14df95e4ed289
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-07-06 17:36:05 +0200

    finished up the PokedexEntry.swift file as the data model for pokedex entries and created the file for the PokedexManager

---
+++ Pokemon Terminal Game/Data/Pokedex/Model/PokedexEntry.swift
@@ -0,0 +1,8 @@
+// MARK: Pokedex Data Model
+
+/// Model representing a single Pokedex entry with all its core properties.
+struct PokedexEntry {
+    let id: Int
+    let name: String
+    let type: String
+}
