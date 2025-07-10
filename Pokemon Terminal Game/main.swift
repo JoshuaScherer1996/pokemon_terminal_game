441ce5b5097fb5ef3ee22ab7a1b0ebf840ed09b3
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-07-10 18:01:46 +0200

    implemented a small to do for the pokedex bug that can only be fixed once the sace functionality is implemented

--- Pokemon Terminal Game/main.swift
+++ Pokemon Terminal Game/main.swift
@@ -4,3 +4,4 @@ let starter = PokeFactory.allPokemon()[0]   // dummy starter
 GameCoordinator(starter: starter).run()
 
 // TODO: Remove the dummy data and implement proper data handling at the start of the game
+// TODO: As soon as the save functionality fix the bug that, when you exit to the main menu the cuaght pokemon stay the same if you start a new game -> Solution could be saving the current caught Pokemon and then clearing them.
