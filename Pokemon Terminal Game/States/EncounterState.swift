94443648ab39a040d26b75c7c2246445c63ada8f
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2025-07-17 16:27:58 +0200

    created the basic outline of the turn based system with a simple while loop and some todo's inisde the Encounter State

--- Pokemon Terminal Game/States/EncounterState.swift
+++ Pokemon Terminal Game/States/EncounterState.swift
@@ -9,53 +9,64 @@ struct EncounterState: GameState {
         
         io.print(Messages.wildEncounterMessage(for: enemyPokemon))
         
-        // TODO: this input logic will be implemented into the user turn in the turn based system
-        // Display the battle menu
-        io.print(Messages.battleMenu)
-        let choice = io.waitFor(
-            options: ["1", "2", "3", "4"],
-            prompt: Messages.inputPromptNumbers
-        )
+        var isRunning: Bool = true
+        
+        while isRunning {
+            
+            // Display the battle menu
+            io.print(Messages.battleMenu)
+            let choice = io.waitFor(
+                options: ["1", "2", "3", "4"],
+                prompt: Messages.inputPromptNumbers
+            )
 
-        switch choice {
-        case "1":
-            io.print(
-                    """
-                    
-                    Coming soon...
-                    """
-            )  // Placeholder
-            return .stay
+            switch choice {
+            case "1":
+                //TODO: Implement attack
+                io.print(
+                        """
+                        
+                        Coming soon...
+                        """
+                )  // Placeholder
+                return .stay
 
-        case "2":
-            io.print(
-                    """
-                    
-                    Coming soon...
-                    """
-            )  // Placeholder
-            return .stay
+            case "2":
+                //TODO: Implement capture
+                io.print(
+                        """
+                        
+                        Coming soon...
+                        """
+                )  // Placeholder
+                return .stay
 
-        case "3":
-            io.print(
-                    """
-                    
-                    Coming soon...
-                    """
-            )  // Placeholder
-            return .stay
+            case "3":
+                //TODO: Implement switch pokemon
+                io.print(
+                        """
+                        
+                        Coming soon...
+                        """
+                )  // Placeholder
+                return .stay
 
-        case "4":
-            io.print(
-                    """
-                    
-                    Coming soon...
-                    """
-            )  // Placeholder
-            return .stay
+            case "4":
+                //TODO: Implement flee
+                io.print(
+                        """
+                        
+                        Coming soon...
+                        """
+                )  // Placeholder
+                return .stay
 
-        default:
-            return .stay
+            default:
+                // Unexpected input should not happen due to waitFor options
+                return .stay
+            }
+            
+            //TODO: Enemy turn here
         }
     }
 }
